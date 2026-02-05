82d1fd46cda97e25bf3a7ab9f6eef53bd226d546
2015kaito8169 <student1259@school.tahara.ed.jp>
2026-02-05 12:57:58 +0900

    Update TypingGame.cs

--- Assets/TypingGame.cs
+++ Assets/TypingGame.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
 using System.Collections.Generic;
 
 public class TypingGame : MonoBehaviour {
     public Text wordText;
     public Text statsText;
-
-    private string[] displayWords = { "信号", "地下鉄", "電気", "ビル", "工事" };
     private string[] romaWords = { "singou", "tikatetu", "denki", "biru", "kouji" };
+    private string[] displayWords = { "信号", "地下鉄", "電気", "ビル", "工事" };
     private int currentIdx;
     private string typedStr = "";
     private float power = 100f;
@@ -39,7 +40,7 @@ public class TypingGame : MonoBehaviour {
         wordText.fontSize = 50;
         wordText.alignment = TextAnchor.MiddleCenter;
         wordText.color = Color.cyan;
-        wordText.rectTransform.anchoredPosition = new Vector2(0, 0);
+        wordText.rectTransform.anchoredPosition = Vector2.zero;
         wordText.rectTransform.sizeDelta = new Vector2(800, 200);
 
         GameObject statsObj = new GameObject("StatsText");
@@ -54,15 +55,10 @@ public class TypingGame : MonoBehaviour {
 
     void Update() {
         if (!isPlaying) return;
-        power -= Time.deltaTime * 2f;
+        power -= Time.deltaTime * 2.5f;
         if (power <= 0) GameOver();
-        CheckInput();
-        UpdateUI();
-    }
-
-    void CheckInput() {
-        string target = romaWords[currentIdx];
         foreach (char c in Input.inputString) {
+            string target = romaWords[currentIdx];
             if (typedStr.Length < target.Length && target[typedStr.Length] == c) {
                 typedStr += c;
                 if (typedStr == target) {
@@ -72,6 +68,8 @@ public class TypingGame : MonoBehaviour {
                 }
             }
         }
+        wordText.text = typedStr + romaWords[currentIdx].Substring(typedStr.Length) + "\n" + displayWords[currentIdx];
+        statsText.text = "POWER: " + Mathf.Floor(power) + "% | SCORE: " + score;
     }
 
     void NextWord() {
@@ -79,16 +77,23 @@ public class TypingGame : MonoBehaviour {
         typedStr = "";
     }
 
-    void UpdateUI() {
-        wordText.text = typedStr + romaWords[currentIdx].Substring(typedStr.Length) + "\n" + displayWords[currentIdx];
-        statsText.text = "POWER: " + Mathf.Floor(power) + "% | SCORE: " + score;
-    }
-
     void GameOver() {
         isPlaying = false;
         wordText.text = "BLACKOUT";
         wordText.color = Color.red;
+        // unityroom API 直接送信（ここにあなたのAPIキーを貼り付けてください）
         string apiKey = "/sSp3O+cEf5Bco80+ESi+TmpCwxlZ0ndsywZzuJzumCrOkkZmSgk5ueG7yYws2j8h0RhIvtU3f7AyxOzWuakRg==";
-        UnityroomApiClient.Instance.SendScore(1, (float)score, ScoreboardWriteMode.HighScoreDesc, apiKey);
+        StartCoroutine(PostScore(1, (float)score, apiKey));
+    }
+
+    IEnumerator PostScore(int boardNo, float scoreVal, string key) {
+        string url = "https://unityroom.com" + boardNo + "/scores";
+        WWWForm form = new WWWForm();
+        form.AddField("score", scoreVal.ToString());
+        form.AddField("mode", "high_score_desc");
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form)) {
+            www.SetRequestHeader("Authorization", "Bearer " + key);
+            yield return www.SendWebRequest();
+        }
     }
 }
